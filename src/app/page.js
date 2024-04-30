8db1311a4e185d99cccb1b1deb18f0942f87d310
unknown <ihourquescos@gmail.com>
2024-04-30 13:26:16 -0300

    agrego video

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
 "use client";
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-	const videoRef = useRef(null);
+	const [showSplash, setShowSplash] = useState(true);
 	const [isPlaying, setIsPlaying] = useState(false);
+
+	const videoRef = useRef(null);
+
+	// useEffect(() => {
+	// 	const timer = setTimeout(() => {
+	// 		setShowSplash(false);
+	// 	}, 5000); // 5000 milliseconds = 5 seconds
+
+	// 	return () => clearTimeout(timer);
+	// }, []); // Run only once on component mount
 
 	const handlePlay = () => {
 		setIsPlaying(true);
@@ -13,17 +23,28 @@ export default function Home() {
 
 	return (
 		<main className={styles.main}>
-			<div className={styles.videoContainer}>
-				<video ref={videoRef} className={styles.video} controls={false}>
-					<source src="../../intro2.mp4" type="video/mp4" />
-					Your browser does not support the video tag.
-				</video>
-				{!isPlaying && (
-					<button className={styles.playButton} onClick={handlePlay}>
-						Empezar
+			{showSplash && (
+				<div className={styles.splashContainer}>
+					<h1 className={styles.title}>
+						<span className={styles.blackText}>presta</span>
+						<span className={styles.redText}>MITO</span>
+					</h1>
+					<p className={styles.description}>
+						pequeños PRESTAMOS para pequeños EMPRENDEDORES
+					</p>
+					<button className={styles.startButton} onClick={handlePlay}>
+						Comenzar
 					</button>
-				)}
-			</div>
+				</div>
+			)}
+			{isPlaying && (
+				<div className={styles.videoContainer}>
+					<video ref={videoRef} className={styles.video} autoPlay>
+						<source src="../../intro2.mp4" type="video/mp4" />
+						Your browser does not support the video tag.
+					</video>
+				</div>
+			)}
 		</main>
 	);
 }
