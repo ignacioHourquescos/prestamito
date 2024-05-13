3342e1b95260a7ba095b6c7d5058dd4d9d581eb8
unknown <ihourquescos@gmail.com>
2024-05-13 00:06:42 -0300

    mejor flujo preguntas

--- src/app/components/questions/components/webCam.js
+++ src/app/components/questions/components/webCam.js
@@ -1,13 +1,18 @@
-import React, { useCallback, useRef, useState } from "react";
+// WebcamVideo.js
+
+import React, { useCallback, useRef, useState, useEffect } from "react";
 import Webcam from "react-webcam";
 import style from "./webCam.module.css";
 
-export default function WebcamVideo(videoRecordedHandler) {
+export default function WebcamVideo({ resetAnimation }) {
 	const webcamRef = useRef(null);
 	const mediaRecorderRef = useRef(null);
 	const [capturing, setCapturing] = useState(false);
 	const [recordedChunks, setRecordedChunks] = useState([]);
 	const [showAnimation, setShowAnimation] = useState(false);
+	const [blink, setBlink] = useState(false);
+	const [completed, setCompleted] = useState(false);
+	const [counter, setCounter] = useState(30); // Inicializar el contador en 30 segundos
 
 	const handleDataAvailable = useCallback(
 		({ data }) => {
@@ -21,6 +26,8 @@ export default function WebcamVideo(videoRecordedHandler) {
 	const handleStartCaptureClick = useCallback(() => {
 		setCapturing(true);
 		setShowAnimation(true);
+		setCompleted(false);
+		setCounter(30); // Reiniciar el contador cuando se inicia la captura
 		mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
 			mimeType: "video/webm",
 		});
@@ -36,6 +43,31 @@ export default function WebcamVideo(videoRecordedHandler) {
 		setCapturing(false);
 		setShowAnimation(false);
 	}, [mediaRecorderRef, setCapturing]);
+
+	useEffect(() => {
+		if (showAnimation) {
+			const timeout = setTimeout(() => {
+				setShowAnimation(false);
+				setCompleted(true);
+			}, 30000); // 30 seconds
+			const interval = setInterval(() => {
+				setBlink((prev) => !prev);
+				setCounter((prev) => prev - 1); // Reducir el contador en 1 cada segundo
+			}, 1000); // Actualizar cada segundo
+			return () => {
+				clearInterval(interval);
+				clearTimeout(timeout);
+			};
+		}
+	}, [showAnimation]);
+
+	useEffect(() => {
+		if (resetAnimation) {
+			setShowAnimation(false);
+			setCompleted(false);
+			setCounter(30);
+		}
+	}, [resetAnimation]);
 
 	const handleDownload = useCallback(() => {
 		if (recordedChunks.length) {
@@ -71,34 +103,62 @@ export default function WebcamVideo(videoRecordedHandler) {
 						ref={webcamRef}
 						videoConstraints={videoConstraints}
 					/>
+					{showAnimation && !completed && (
+						<div className={style.animationOverlay}>
+							<div className={style.notificationWrapperRed}>
+								<span
+									className={`${style.redCircle} ${blink ? style.blink : ""}`}
+								></span>
+								<span className={style.recordingText}>Grabando</span>
+							</div>
+						</div>
+					)}
+					{completed && (
+						<div className={style.animationOverlay}>
+							<div className={style.notificationWrapperGreen}>
+								<span className={style.completedText}>✅</span>
+								<span className={style.completedText}>
+									Video capturado con éxito
+								</span>
+							</div>
+						</div>
+					)}
 				</div>
 				{showAnimation && <div className={style.redLine}></div>}
 			</div>
-			<div style={{ width: "100%" }}>
+
+			<div
+				style={{
+					width: "100%",
+					textAlign: "center",
+					fontSize: "2rem",
+					fontWeight: "bold",
+				}}
+			>
 				{capturing ? (
 					<button
-						className={style.startButton2}
+						className={`${style.startButton2} ${style.stopButton}`}
 						onClick={handleStopCaptureClick}
 					>
-						Stop Capture
+						Reiniciar
 					</button>
 				) : (
 					<button
 						className={style.startButton2}
 						onClick={() => {
 							handleStartCaptureClick();
-							videoRecordedHandler();
 						}}
 					>
-						Start Capture
+						Iniciar captura
 					</button>
 				)}
 			</div>
+			{capturing && <div className={style.counter}>{counter} segundos</div>}
 			{/* {recordedChunks.length > 0 && (
-				<button className={style.startButton2} onClick={handleDownload}>
-					Download
-				</button>
-			)} */}
+                <button className={style.startButton2} onClick={handleDownload}>
+                    Download
+                </button>
+            )} */}
 		</div>
 	);
 }
--- src/app/components/questions/question.js
+++ src/app/components/questions/question.js
@@ -1,3 +1,5 @@
+// DotsMobileStepper.js
+
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import MobileStepper from "@mui/material/MobileStepper";
@@ -11,9 +13,12 @@ import styles from "./question.module.css";
 export default function DotsMobileStepper() {
 	const theme = useTheme();
 	const [activeStep, setActiveStep] = React.useState(0);
+	const [resetAnimation, setResetAnimation] = React.useState(false);
+	const [capturing, setCapturing] = React.useState(false);
 
 	const handleNext = () => {
 		setActiveStep((prevActiveStep) => prevActiveStep + 1);
+		setResetAnimation(true);
 	};
 
 	const questions = [
@@ -34,22 +39,82 @@ export default function DotsMobileStepper() {
 
 	const handleBack = () => {
 		setActiveStep((prevActiveStep) => prevActiveStep - 1);
+		setResetAnimation(true);
+	};
+
+	const stepTexts = [
+		{
+			title: "Acerca de vos",
+			questions: [
+				"¿Hace cuántos años comenzaste?",
+				"¿Cuántas personas trabajan?",
+				"¿Tienes otro trabajo aparte?",
+			],
+		},
+		{
+			title: "Acerca de tu emprendimiento",
+			questions: [
+				"¿Nos podrías hacer un breve video mostrando tus herramientas de trabajo (mobiliario, herramientas, mercadería, etc)?",
+			],
+		},
+
+		{
+			title: "Acerca de tus finanzas",
+			questions: [
+				"¿Nos podrías contar cómo manejas las finanzas de tu emprendimiento?",
+				"¿Cómo llevas los números? (en un cuaderno, en una planilla, etc..)",
+			],
+		},
+		{
+			title: "Acerca del préstamo",
+			questions: [
+				"Por último,",
+				"¿Para qué utilizarías el dinero del préstamo?",
+			],
+		},
+		{
+			title: "Gracias",
+			questions: ["Nos conctataremos con vo via Whatsapp"],
+		},
+	];
+
+	// Restablecer resetAnimation cada vez que cambia el paso activo
+	React.useEffect(() => {
+		setResetAnimation(false);
+	}, [activeStep]);
+
+	const handleStartCaptureClick = () => {
+		setCapturing(true);
+	};
+
+	const handleStopCaptureClick = () => {
+		setCapturing(false);
+		setResetAnimation(true);
 	};
 
 	return (
-		<div className={styles.container}>
+		<div className={`${styles.container} ${styles.koho_regular}`}>
+			{activeStep % 2 == 0 && (
+				<div className={styles.introStep}>
+					{stepTexts[Math.floor(activeStep / 2)].title} {/* Título */}
+					{/* Preguntas debajo del título */}
+					{stepTexts[Math.floor(activeStep / 2)].questions.map(
+						(question, index) => (
+							<div className={styles.questions_specific} key={index}>
+								{question}
+							</div>
+						)
+					)}
+				</div>
+			)}
 			<MobileStepper
 				variant="dots"
-				steps={questions.length}
+				steps={8}
 				position="static"
 				activeStep={activeStep}
 				sx={{ maxWidth: 600, flexGrow: 1 }}
 				nextButton={
-					<Button
-						size="small"
-						onClick={handleNext}
-						disabled={activeStep === questions.length - 1}
-					>
+					<Button size="small" onClick={handleNext} disabled={activeStep === 8}>
 						Siguiente
 						{theme.direction === "rtl" ? (
 							<KeyboardArrowLeft />
@@ -69,15 +134,25 @@ export default function DotsMobileStepper() {
 					</Button>
 				}
 			/>
-			<WebcamVideo />
-			<div className={styles.questions}>
-				{questions[activeStep].map((question, index) => (
-					<React.Fragment key={index}>
-						<div className={styles.questions_specific}>{question}</div>
-						{index !== questions[activeStep].length - 1}
-					</React.Fragment>
-				))}
-			</div>
+			<WebcamVideo
+				resetAnimation={resetAnimation}
+				capturing={capturing}
+				onStartCaptureClick={handleStartCaptureClick}
+				onStopCaptureClick={handleStopCaptureClick}
+			/>
+			{/* <div className={styles.questions}>
+            {activeStep % 2 === 0 ? (
+              questions[activeStep].map((question, index) => (
+                <div key={index} className={styles.questions_specific}>
+                  {question}
+                </div>
+              ))
+            ) : (
+              <div className={styles.questions_specific}>
+                {questions[activeStep]}
+              </div>
+            )}
+          </div> */}
 		</div>
 	);
 }
